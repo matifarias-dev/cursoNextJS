0172ba2b62a7d61c100f4ff9c85163d73bcb17ce
Matt <maty.farias@hotmail.com>
2024-07-31 21:03:44 -0300

    statics

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,3 +1,17 @@
+import styles from './Description.module.sass'
+
 export const Description = () => {
-  return <h3>Description</h3>
+  return (
+    <section className={styles.Description}>
+      <img src='/images/description.jpeg' alt='imagen futurista' />
+      <div className={styles.Description__text}>
+        <h2>Bring the future today</h2>
+        <p>
+          Future World: Your Gateway to Tomorrow's Tech! Dive into a world of
+          cutting-edge gadgets and gear. Stay ahead of the curve and redefine
+          your digital lifestyle with us.
+        </p>
+      </div>
+    </section>
+  )
 }
