da172238b0912ca275facc961bf90502f77eee01
Matt <maty.farias@hotmail.com>
2024-08-14 09:04:38 -0300

    statics e imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import styles from './Description.module.sass'
 
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <img src='/images/description.jpeg' alt='imagen futurista' />
+      <Image
+        src='/images/description.jpeg'
+        alt='imagen futurista'
+        width={500}
+        height={300}
+        priority={true}
+      />
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
         <p>
